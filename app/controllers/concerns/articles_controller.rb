cfd607d5f629a4e51a8a19b1666a4e88a1ca1e4b
jaephillseo <jaephillseo@gmail.com>
2017-02-12 23:16:29 -0800

    Added Layout links to books database

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
 	def new
 		@article = Article.new
+	end
+
+	def edit
+		@article = Article.find(params[:id])
 	end
 
 	def create
