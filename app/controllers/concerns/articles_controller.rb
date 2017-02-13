d874138d5387144a9958072b8c97d466e4630bf5
jaephillseo <jaephillseo@gmail.com>
2017-02-12 22:38:55 -0800

    Created authentication for new postings, and fixed controllers for article

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -4,6 +4,24 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		render plain: params[:article].inspect
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = "Post was successfully created"
+			redirect_to article_path(@article)
+		else
+			render 'new'
+		end
 	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+
+	private
+		def article_params
+			#permit article to change title and description
+			params.require(:article).permit(:title, :description)
+		end
+
 end
