e026ea2f2f9b9e5b04820657fb0193eb25314cae
YONGYONG YUAN <josephyuany@163.com>
2017-02-25 05:20:19 +0000

    finished creating user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   #get 'welcome/home' #it can be deleted, but have to use root_path
+  resources :users
 
   
   get '/help', to: 'welcome#help'
@@ -10,7 +11,11 @@ Rails.application.routes.draw do
   get '/contact', to:'welcome#contact'
   
   get '/signup', to: 'users#new'
+  
+  post '/signup', to: 'users#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
+  
+  
 end
