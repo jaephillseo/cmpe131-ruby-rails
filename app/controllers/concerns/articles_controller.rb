e4ce473f71314eb0c71b3c6e00c9610922353f6d
jaephillseo <jaephillseo@gmail.com>
2017-03-01 18:15:31 -0800

    Added delete button for books form

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -25,6 +25,13 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		flash[:notice] = "Listing was successfully deleted"
+		redirect_to articles_path
+	end
+
 
 	private
 		def article_params
