3f1d2f76b803b701aae911ffbeabef11dbf76baf
jaephillseo <jaephillseo@gmail.com>
2017-05-11 02:53:05 -0700

    Update all views with categories

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -50,7 +50,7 @@ class ArticlesController < ApplicationController
 	private
 		def article_params
 			#permit article to change title and description
-			params.require(:article).permit(:title, :description)
+			params.require(:article).permit(:title, :description, category_ids: [])
 		end
 
 		def set_article
